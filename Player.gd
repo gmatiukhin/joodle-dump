f05ef58d13bc790e9b67290c1c13a97a15fe79f2
Grigorii Matiukhin <grigory36m@gmail.com>
2023-03-04 00:42:37 +0300

    Better player controls

--- Player.gd
+++ Player.gd
@@ -1,35 +1,48 @@
 extends KinematicBody2D
 
-export var g = 100
-export var jump_speed = 200
-export var duck_speed = 100
-export var movement_speed = 100
+const WALK_SPEED = 100
+const WALK_MAX_SPEED = 200
 
-var charge = 0
-export var charge_speed = 1
+const JUMP_SPEED = 200
+const JUMP_DECELERATION = 1.1
+const FALL_ACCELERATION = 5
 
+const CHARGE_MODIFIER = 1.03
+# Stores charge between frames
+var charge = 1
 
+const DUCK_SPEED = 100
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var velocity = Vector2()
 
+onready var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	var velocity = Vector2.ZERO
-	velocity.y += g
-	if is_on_floor():
-		if Input.is_action_pressed("jump"):
-			charge += charge_speed
-		if Input.is_action_just_released("jump"):
-			velocity.y -= jump_speed * charge
-			charge = 0
-	if Input.is_action_just_pressed("duck"):
-		velocity.y += duck_speed
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= movement_speed
-	if Input.is_action_pressed("move_right"):
-		velocity.x += movement_speed
+	# Horizontal movement code. First, get the player's input.
+	var walk = WALK_SPEED * (Input.get_action_strength("move_right") - Input.get_action_strength("move_left"))
+	# Slow down the player if they're not trying to move.
+	position.x += walk * delta
 	
-	move_and_slide(velocity, Vector2.UP)
+	# Jumping and ducking controls
+	if is_on_floor():
+		print(charge)
+		if Input.is_action_pressed("jump"):
+			charge *= CHARGE_MODIFIER
+			charge = clamp(charge, 1, 4)
+		if Input.is_action_just_released("jump"):
+			velocity.y = -JUMP_SPEED * charge
+			charge = 1
+	else:
+		if Input.is_action_just_pressed("duck"):
+			velocity.y += DUCK_SPEED
+	
+	# Vertical acceleration/deceleration code. Apply gravity.
+	if velocity.y < 0: # Juming
+		# Start jump at full speed but slow down in the air
+		velocity.y /= JUMP_DECELERATION
+	velocity.y += gravity * FALL_ACCELERATION * delta
+	
+	# Move based on the velocity and snap to the ground.
+	velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
+	# Wrap x position
+	position.x = wrapf(position.x, 0, get_viewport_rect().size.x)
